669461c7eed449883e827fc265196ea5f873614b
mariellenolasco <mariellenolasco@gmail.com>
2019-10-01 11:48:33 -0500

    demo program from first day

---
+++ HelloRevature/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelloRevature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Account a = new Account();
+            a.Withdraw(5);
+        }
+    }
+    class Account
+{
+    decimal balance;
+    private readonly object sync = new object();
+    public void Withdraw(decimal amount) 
+    {
+        lock (sync) 
+        {
+            if (amount > balance) 
+            {
+                throw new Exception(
+                    "Insufficient funds");
+            }
+            balance -= amount;
+        }
+    }
+}
+}
