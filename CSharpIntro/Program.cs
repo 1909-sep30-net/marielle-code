b402babc0e1b5e92c1bd214fba91edebd13f2cf2
mariellenolasco <mariellenolasco@gmail.com>
2019-10-01 12:30:29 -0500

    intro to csharp files

---
+++ CSharpIntro/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CSharpIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            bool a = true;
+            String b = "Marielle Nolasco";
+            int c = 1;
+            a = false;
+            b = "Marielle Carmina Nolasco";
+            c++;
+            Console.WriteLine(a + b + c);
+            for (int i = 0; i < c; i++)
+            {
+                Console.WriteLine(i);
+            }
+            while (!a)
+            {
+                Console.WriteLine(a);
+                a = !a;
+            }
+            Random rnd = new Random();
+            int n = rnd.Next(1, 4);
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("Hello");
+                    break;
+                case 2:
+                    Console.WriteLine("Goodbye");
+                    break;
+
+            }
+            if (n == 1)
+            {
+                Console.WriteLine(" I'm");
+            }
+            else if (n == 2)
+            {
+                Console.WriteLine(" See");
+            }
+            else
+            {
+                Console.WriteLine(" Sir");
+            }
+            /*
+            This 
+            I hope 
+            is 
+            a 
+            multi line 
+            comment
+             */
+             greet("Marielle");
+        }
+        static void greet(string s){
+            Console.WriteLine("Nice to meet you " + s);
+        }
+    }
+}
