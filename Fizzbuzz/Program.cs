f9ed242f270b61d6e097ff2921bf1cc109261d2b
mariellenolasco <mariellenolasco@gmail.com>
2019-10-02 11:21:39 -0500

    Fixed the fizzbuzz code a bit

--- Fizzbuzz/Program.cs
+++ Fizzbuzz/Program.cs
@@ -6,26 +6,23 @@ namespace Fizzbuzz
     {
         static void Main(string[] args)
         {
-            int ctr1 = 0;
-            int ctr2 = 0;
-            int ctr3 = 0;
             for(int i = 1; i<1001; i++){
                 if(i%15 == 0){
                     Console.WriteLine("Fizzbuzz");
-                    ctr1++;
+                    
                 } else if(i%3 == 0){
                     Console.WriteLine("Fizz");
-                    ctr2++;
+                    
                 } else if (i%5 == 0){
                     Console.WriteLine("Buzz");
-                    ctr3++;
+                    
                 } else {
                     Console.WriteLine(i);
                 }
             }
-            Console.WriteLine("Total number of Fizz: " + ctr2);
-            Console.WriteLine("Total number of Buzz: " + ctr3);
-            Console.WriteLine("Total number of FizzBuzz: " + ctr1);
+            Console.WriteLine("Total number of Fizz: " + (1000/3 - 1000/15));
+            Console.WriteLine("Total number of Buzz: " + (1000/5 - 1000/15));
+            Console.WriteLine("Total number of FizzBuzz: " + 1000/15);
         }
     }
 }
